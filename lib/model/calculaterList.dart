fad1b68008975ce97d61224f327ca490c2b2d5b1
John Doe <yakub5896@gmail.com>
2021-04-13 19:39:47 +0300

    commit

--- lib/model/calculaterList.dart
+++ lib/model/calculaterList.dart
@@ -2,7 +2,9 @@ import 'calculater_model.dart';
 import '../controller/VKİ.dart';
 import '../controller/GlascowComa.dart';
 
-List<Calculater> calculaterList = [
-  vki,
-  glasgowComaScale,
-];
+List<Calculater> calculaterList = [vki, glasgowComaScale, wells];
+
+Calculater wells = Calculater(
+  title: 'Wells kriterleri',
+  subTitle: 'DVT riski hesaplama',
+);
